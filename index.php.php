77f22dd5f6fab7035327e4c5d59d26721635b476
ThugTheCreator <vini9296@gmail.com>
2018-08-26 13:34:42 -0300

    Teste
    ..

---
+++ index.php.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Projeto1 PHP</title>
+    <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        print "Olá Mundo!";
+        $nome = "ThugVini";
+        print "Meu nickname é " . $nome;
+
+        
+        ?>
+    </body>
+
+
+
+
+</html>
